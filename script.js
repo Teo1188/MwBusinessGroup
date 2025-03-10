b0c7fb24de13734e91a49918ca478d9a5b910c21
Tadeo Gaviria <tadeogaviria@gmail.com>
2025-03-09 22:21:33 -0500

    3er intento ruta relativa pages

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function getCurrentPage() {
     const path = window.location.pathname; // Obtiene la ruta del archivo actual
 
     // Si la ruta es la raíz (/) o contiene 'index.html', devuelve 'index'
-    if (path === '/' || path.endsWith('index.html') || path === 'MwBusinessGroup/') {
+    if (path === '/' || path.endsWith('index.html') || path.endsWith('/MwBusinessGroup/')) {
         return 'index';
     }
 
